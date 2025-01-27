2f484a6104975a4f7478d0e56f42ac744ca33448
DESKTOP-6H1BQMR\dlxog <lss8314@naver.com>
2025-01-27 17:47:14 +0900

    dispatcherServlet 오류사항 수정

--- projectWep/src/main/java/com/example/controller/DispatcherServlet.java
+++ projectWep/src/main/java/com/example/controller/DispatcherServlet.java
@@ -4,15 +4,16 @@ import javax.servlet.*;
 import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServlet;
 
-public class DispatcherServlet {
+public class DispatcherServlet extends HttpServlet {
 	
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         processRequest(request, response);
     }
 
-	//@Override
+	@Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         processRequest(request, response);
     }
